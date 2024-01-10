1a8cd837d9e11b0d27a6fb4e51a548d5b3f09987
xAnt01x <antluisi2001@gmail.com>
2024-01-10 14:18:05 +0100

    commit project

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oae0eb42jM6cllak5d7',
+        issuer: 'https://dev-94441402.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
